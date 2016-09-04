ca35f68084df1f6f5073d4fd3b4bdfc6a2e918a8
emmaodia <odia.emma@gmail.com>
2016-09-04 02:55:58 +0100

    Added static pages: about, contact and faq to welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  get 'welcome/faq'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
